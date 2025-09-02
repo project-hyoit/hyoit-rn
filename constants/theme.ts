a7e9e1250b84b456101a005815085855034401f7
bom0320 <s24040@gsm.hs.kr>
2025-09-02 16:54:14 +0900

    refactor: remove Colors.ts and unify theme config

--- constants/theme.ts
+++ constants/theme.ts
@@ -2,22 +2,20 @@ import {
   DarkTheme as RNDark,
   DefaultTheme as RNLight,
 } from "@react-navigation/native";
-import { Colors } from "./Colors";
 
-// Color.ts와 React Navigation 병합
 export const LightTheme = {
   ...RNLight,
   colors: {
     ...RNLight.colors,
-    primary: Colors.light.tint,
-    background: Colors.light.background,
-    card: Colors.light.background,
-    text: Colors.light.text,
+    primary: "#0a7ea4",
+    background: "#fff",
+    card: "#fff",
+    text: "#11181C",
     border: "#E5E7EB",
-    notification: Colors.light.tint,
-    icon: Colors.light.icon,
-    tabIconDefault: Colors.light.tabIconDefault,
-    tabIconSelected: Colors.light.tabIconSelected,
+    notification: "#0a7ea4",
+    icon: "#687076",
+    tabIconDefault: "#687076",
+    tabIconSelected: "#0a7ea4",
   },
 };
 
@@ -25,14 +23,14 @@ export const DarkTheme = {
   ...RNDark,
   colors: {
     ...RNDark.colors,
-    primary: Colors.dark.tint,
-    background: Colors.dark.background,
-    card: Colors.dark.background,
-    text: Colors.dark.text,
+    primary: "#fff",
+    background: "#151718",
+    card: "#151718",
+    text: "#ECEDEE",
     border: "#374151",
-    notification: Colors.dark.tint,
-    icon: Colors.dark.icon,
-    tabIconDefault: Colors.dark.tabIconDefault,
-    tabIconSelected: Colors.dark.tabIconSelected,
+    notification: "#fff",
+    icon: "#9BA1A6",
+    tabIconDefault: "#9BA1A6",
+    tabIconSelected: "#fff",
   },
 };
