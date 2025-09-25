8bd796c6110eb0f5ee1d6cd1cc60383bbae4ff93
bom0320 <s24040@gsm.hs.kr>
2025-09-25 19:49:20 +0900

    remove: unnecessary folder

--- pages/login/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function LoginScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>로그인 화면입니다</Text>
-    </View>
-  );
-}
